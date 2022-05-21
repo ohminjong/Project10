9623f4d6c25173fc5fe241992bdb2b410446d70b
ohminjong <ohmj1999@naver.com>
2022-05-21 21:55:01 +0900

    게시판 생성

--- project/urls.py
+++ project/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include("users.urls"))
+    path('auth/', include("users.urls")),
+    path('board/', include('board.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
